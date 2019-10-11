3bd69474a61a3792e8124f83f2577558850e7c86
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-10-11 15:26:28 +0200

    add padStart

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ $('form').on('submit', (event) => {
   event.preventDefault();
   const inputValue = $('.write').val();
   const date = new Date($.now());
-  const formatted = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  const formatted = date.getHours().toString().padStart(2, 0) + ":" + date.getMinutes().toString().padStart(2, 0) + ":" + date.getSeconds().toString().padStart(2, 0);
+  console.log(formatted);
   const task = `<li><span class="number">Tasks number ${tasks.length + 1}${inputValue ? ":" : ""} ${inputValue} </span><span class="time">${formatted}</span></li>`;
   $('ul').append(task);
   tasks.push(task)
