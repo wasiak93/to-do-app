69ebd11f52c98d7420bb0274f6aec0fda86a27ca
Kacper Wasiak <kacperjacekwasiak@gmail.com>
2019-10-11 13:58:59 +0200

    remove span from task

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $('form').on('submit', (event) => {
   const inputValue = $('.write').val();
   const date = new Date($.now());
   const formatted = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-  const task = $(`<li><span class="number">Tasks number<span> ${tasks.length + 1}: </span></span><span class="add-task"> ${inputValue} </span><span class="time">${formatted}</span></li>`);
+  const task = $(`<li><span class="number">Tasks number ${tasks.length + 1}${inputValue ? ":" : ""} ${inputValue} </span><span class="time">${formatted}</span></li>`);
   $('ul').append(task);
   tasks.push(task)
   $('.write').val('');
